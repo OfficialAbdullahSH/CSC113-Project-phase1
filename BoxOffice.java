882ba590e9f5d52fea92d1e41f0e98c485381722
OfficialAbdullahSH <444102864@student.ksu.edu.sa>
2024-02-26 11:04:48 +0300

    Added Methods and attributes in BoxOffice class

--- BoxOffice.java
+++ BoxOffice.java
@@ -1,2 +1,36 @@
 public class BoxOffice {
+    private Movie [] movies = new Movie[20];
+    private int numberOfMovies;
+    private int numberOfTickets;
+
+    public BoxOffice() {
+    }
+
+    public Movie[] getMovies() {
+        return movies;
+    }
+
+    public boolean addMovie(){
+
+    }
+
+    public boolean removeMovie(){
+
+    }
+
+    public boolean Booking(){
+
+    }
+
+    public boolean cancleBooking(){
+
+    }
+
+    public Movie searchMovie (String name){
+
+    }
+
+    public void display(){
+
+    }
 }
