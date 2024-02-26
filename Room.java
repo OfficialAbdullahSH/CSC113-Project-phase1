08c2838ae3ff793423de7f5e6e7042869834e9cd
OfficialAbdullahSH <444102864@student.ksu.edu.sa>
2024-02-26 11:02:10 +0300

    Added Methods and attributes in Room class

--- Room.java
+++ Room.java
@@ -1,2 +1,22 @@
 public class Room {
+
+    private int roomNumber;
+    private int capacity;
+
+    public Room(int roomNumber, int capacity) {
+        this.roomNumber = roomNumber;
+        this.capacity = capacity;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void display(){
+
+    }
 }
