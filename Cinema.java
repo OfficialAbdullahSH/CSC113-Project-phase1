d4f641a1e7847ca2efbd26ea25ec72c670003d20
OfficialAbdullahSH <444102864@student.ksu.edu.sa>
2024-02-26 10:33:34 +0300

    Added Methods and attributes in Cinema class

--- Cinema.java
+++ Cinema.java
@@ -1,2 +1,51 @@
-public class Cinema {
+
+
+
+
+
+public class Cinema extends Company {
+
+    private String address;
+
+    private Room [] arrRooms = new Room[15];
+    private int roomsCounter;
+    private BoxOffice [] arrBoxOffice = new BoxOffice[3];
+    private int boxOfficeCounter;
+
+    public Cinema(String name, String address) {
+        super(name);
+
+        this.address = address;
+    }
+
+    public Cinema(Cinema c) { // Copy constructor
+        super(c.name);
+
+        this.address = c.address;
+    }
+
+
+    @Override
+    public String getName(){
+        return name;
+    }
+
+    public boolean addRoom (Room room){
+
+    }
+
+    public boolean removeRoom (Room room){
+
+    }
+
+    public Room[] getRooms() {
+
+        return arrRooms;
+    }
+
+    public void display(){
+
+    }
 }
+
+
