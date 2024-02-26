ba36fa28a22fe30c4f4b73bb9e3129f7108fc37b
OfficialAbdullahSH <444102864@student.ksu.edu.sa>
2024-02-26 11:08:00 +0300

    Added Methods and attributes in Movie class

--- Movie.java
+++ Movie.java
@@ -1,2 +1,15 @@
 public class Movie {
+    private String movieName;
+    private int movieDuration;
+    private String releaseDate;
+
+    public Movie(String movieName, int movieDuration, String releaseDate) {
+        this.movieName = movieName;
+        this.movieDuration = movieDuration;
+        this.releaseDate = releaseDate;
+    }
+
+    public void display(){
+
+    }
 }
