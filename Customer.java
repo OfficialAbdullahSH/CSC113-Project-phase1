4a97357a825e13c197555c45def2871d76ac44f5
OfficialAbdullahSH <444102864@student.ksu.edu.sa>
2024-02-26 11:10:22 +0300

    Added Methods and attributes in Customer class

--- Customer.java
+++ Customer.java
@@ -1,2 +1,28 @@
 public class Customer {
+
+    private String name;
+    private String email;
+    private int age;
+
+    public Customer(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void display(){
+
+    }
 }
